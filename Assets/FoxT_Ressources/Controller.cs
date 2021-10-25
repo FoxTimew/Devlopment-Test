d0bf465cf057cd3ea2640862cdd4c9f062e9ba4f
FoxTimew <foxtimew@hotmail.com>
2021-10-25 11:36:16 +0200

    Controller Debug

---
+++ Assets/FoxT_Ressources/Controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	private void Awake()
+	{
+		Debug.Log("I'm Awake.");
+	}
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
